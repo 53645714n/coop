6b169e2ca52eedb149655e3a4772b333eede1fb0
53645714n <schoenmaker@mailbox.org>
2021-04-21 22:15:11 +0200

    Added relais and fixed some logging issues

--- coop.py
+++ coop.py
@@ -21,36 +21,23 @@ import RPi.GPIO as GPIO
 import time
 from suntime import Sun
 import threading
-#import urllib.request
-#import pytz
 import os
-
-os.rename('/home/pi/coop.log', '/home/pi/'+datetime.now().strftime("%Y%m%d-%H%M%S")+'coop.log')
 
 # config
 config = configparser.ConfigParser()
 config.read('/home/pi/coop/coop.ini')
 
 # logging
-logging.basicConfig(filename=config['Settings']['LogFile'], level=config['Settings']['LogLevel'],
-                    format='%(asctime)s - %(levelname)s: %(message)s')
+logfile = config['Settings']['LogFile']
+os.rename(logfile, '/home/pi/'+datetime.now().strftime("%Y%m%d-%H%M%S")+'.log') if os.path.isfile(logfile) else print('no logfile found, first time run?')
+logging.basicConfig(filename=logfile, level=config['Settings']['LogLevel'],format='%(asctime)s - %(levelname)s: %(message)s') if logfile != "" else logging.basicConfig(level=config['Settings']['LogLevel'],format='%(asctime)s - %(levelname)s: %(message)s')
 
 # Suntime
 sun = Sun(float(config['Location']['Latitude']), float(config['Location']['Longitude']))
-#tz = pytz.timezone('Europe/Berlin')
-#now = (datetime.now(timezone.utc))
 offsetclose = int(config['Door']['OffsetClose'])
 offsetopen = int(config['Door']['OffsetOpen'])
-#minopen = datetime.strptime(config['Door']['MinOpen'],'%H:%M')
-#minopenlocal = tz.localize(minopen)
-#minopenutc = minopenlocal.astimezone(pytz.utc)
-#opentime = sun.get_sunrise_time() - timedelta(minutes=offsetopen)
-#closetime = sun.get_sunset_time() + timedelta(minutes=offset)
-#opentimetomorrow = sun.get_local_sunrise_time(datetime.now() + timedelta(days=1) - timedelta(minutes=offset))
-#closetimeyesterday = sun.get_local_sunset_time(datetime.now() + timedelta(days=-1)) + timedelta(minutes=offset)
 global stop_threads
 
-#relais
 
 # GPIO
 GPIO.setmode(GPIO.BOARD)
@@ -61,6 +48,10 @@ MotorDown = int(config['GPIO']['ActDwn'])
 GPIO.setup(MotorDown, GPIO.OUT)
 Relais = int(config['GPIO']['Relais'])
 GPIO.setup(Relais, GPIO.OUT)
+RelaisOn = int(config['GPIO']['RelaisOn'])
+GPIO.setup(RelaisOn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+RelaisOff = int(config['GPIO']['RelaisOff'])
+GPIO.setup(RelaisOff, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 SwitchDown = int(config['GPIO']['BttnDwn'])
 GPIO.setup(SwitchDown, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 SwitchUp = int(config['GPIO']['BttnUp'])
@@ -82,20 +73,12 @@ def motor_down():
     GPIO.output(MotorDown, GPIO.HIGH)
     GPIO.output(MotorUp, GPIO.LOW)
 
-def motor_stop():
-    logging.debug("motor_stop")
-    GPIO.output(MotorUp, GPIO.LOW)
-    GPIO.output(MotorDown, GPIO.LOW)
-
 def door():
     global door
     now = (datetime.now(timezone.utc))
     logging.debug("Time is: %s", now)
     logging.debug("Sunrise time today: %s", sun.get_sunrise_time())
     logging.debug("Sunset time today: %s", sun.get_sunset_time())
-#    logging.debug("MinOpen time: %s", minopen)
-#    logging.debug("MinOpenlocal time: %s", minopenlocal)
-#    logging.debug("MinOpenutc time: %s", minopenutc))
     GPIO.add_event_detect(SwitchDown,GPIO.RISING,callback=button_callback, bouncetime=200)
     GPIO.add_event_detect(SwitchUp,GPIO.RISING,callback=button_callback, bouncetime=200)
     while True:
@@ -130,48 +113,133 @@ def door():
                 door = "closed"
                 logging.info("Door status changed: %s", door)
                 logging.info("Opening scheduled at %s", next_open)
+        elif stop_threads:
+            break
 
 def relais():
-#   possible options: door, inverse, time
+    global relaisstatus
+    relaisstatus = "undefined"
     logging.debug("relais started")
     relais = config['Relais']['Mode']
-    logging.debug("relais = %s", relais)
-    global doornew
-    global door
-    doornew = "old"
+    logging.info("Relais set to mode: %s", relais)
+    GPIO.add_event_detect(RelaisOn,GPIO.RISING,callback=button_callback, bouncetime=200)
+    GPIO.add_event_detect(RelaisOff,GPIO.RISING,callback=button_callback, bouncetime=200)
     while True:
-        if str(door) != str(doornew):
-            doornew = str(door)
-#        while True:
-            if relais == "door":
-                logging.info("relais set to 'door'")
-                if door == "closed":
-                    GPIO.output(Relais, GPIO.LOW)
-                    logging.info("relais switched off")
-                elif door == "open":
-                    GPIO.output(Relais, GPIO.HIGH)
-                    logging.info("relais switched on")
-            elif relais == "inverse":
-                logging.info("relais set to 'inverse'")
-                if door == "closed":
-                    GPIO.output(Relais, GPIO.HIGH)
-                    logging.info("relais switched on")
-                elif door == "open":
-                    GPIO.output(Relais, GPIO.LOW)
-                    logging.info("relais switched off")
+        if relais == "door":
+            now = (datetime.now(timezone.utc))
+            minopen = datetime.strptime(datetime.now().strftime("%Y-%m-%d-")+config['Door']['MinOpen']+" +0000",'%Y-%m-%d-%H:%M %z')
+            opentime = minopen if minopen > sun.get_sunrise_time() + timedelta(minutes=offsetopen) else sun.get_sunrise_time() + timedelta(minutes=offsetopen)
+            closetime = sun.get_sunset_time() + timedelta(minutes=offsetclose)
+            opentimetomorrow = minopen + timedelta(days=1) if sun.get_sunrise_time(datetime.now() + timedelta(days=1)) + timedelta(minutes=offsetopen) < minopen + timedelta(days=1) else sun.get_sunrise_time(datetime.now() + timedelta(days=1)) + timedelta(minutes=offsetopen)
+            closetimeyesterday = sun.get_sunset_time(datetime.now() + timedelta(days=-1)) + timedelta(minutes=offsetclose)
+            closetimetomorrow = sun.get_sunset_time(datetime.now() + timedelta(days=+1)) + timedelta(minutes=offsetclose)
+            next_open = opentime if now < opentime else opentimetomorrow
+            next_close = closetime if now < closetime else closetimetomorrow
+            if GPIO.input(RelaisOn) == True:
+                if relaisstatus != "on":
+                    GPIO.output(Relais,GPIO.HIGH)
+                    relaisstatus = "on"
+                    logging.warning("Relais status changed manually: %s", relaisstatus)
+            elif GPIO.input(RelaisOff) == True:
+                if door != "off":
+                    GPIO.output(Relais,GPIO.LOW)
+                    relaisstatus = "off"
+                    logging.warning("Relais status changed manually: %s", relaisstatus)
+            elif opentime < now < closetime:
+                if relaisstatus != "on":
+                    GPIO.output(Relais,GPIO.HIGH)
+                    relaisstatus = "on"
+                    logging.info("Relais status changed: %s", relaisstatus)
+            elif closetimeyesterday < now < opentime or closetime < now < opentimetomorrow:
+                if relaisstatus != "off":
+                    GPIO.output(Relais,GPIO.LOW)
+                    relaisstatus = "off"
+                    logging.info("Relais status changed: %s", relaisstatus)
+        elif relais == "inverse":
+            now = (datetime.now(timezone.utc))
+            minopen = datetime.strptime(datetime.now().strftime("%Y-%m-%d-")+config['Door']['MinOpen']+" +0000",'%Y-%m-%d-%H:%M %z')
+            opentime = minopen if minopen > sun.get_sunrise_time() + timedelta(minutes=offsetopen) else sun.get_sunrise_time() + timedelta(minutes=offsetopen)
+            closetime = sun.get_sunset_time() + timedelta(minutes=offsetclose)
+            opentimetomorrow = minopen + timedelta(days=1) if sun.get_sunrise_time(datetime.now() + timedelta(days=1)) + timedelta(minutes=offsetopen) < minopen + timedelta(days=1) else sun.get_sunrise_time(datetime.now() + timedelta(days=1)) + timedelta(minutes=offsetopen)
+            closetimeyesterday = sun.get_sunset_time(datetime.now() + timedelta(days=-1)) + timedelta(minutes=offsetclose)
+            closetimetomorrow = sun.get_sunset_time(datetime.now() + timedelta(days=+1)) + timedelta(minutes=offsetclose)
+            next_open = opentime if now < opentime else opentimetomorrow
+            next_close = closetime if now < closetime else closetimetomorrow
+            if GPIO.input(RelaisOn) == True:
+                if relaisstatus != "on":
+                    GPIO.output(Relais,GPIO.HIGH)
+                    relaisstatus = "on"
+                    logging.warning("Relais status changed manually: %s", relaisstatus)
+            elif GPIO.input(RelaisOff) == True:
+                if door != "off":
+                    GPIO.output(Relais,GPIO.LOW)
+                    relaisstatus = "off"
+                    logging.warning("Relais status changed manually: %s", relaisstatus)
+            elif opentime < now < closetime:
+                if relaisstatus != "off":
+                    GPIO.output(Relais,GPIO.LOW)
+                    relaisstatus = "off"
+                    logging.info("Relais status changed: %s", relaisstatus)
+            elif closetimeyesterday < now < opentime or closetime < now < opentimetomorrow:
+                if relaisstatus != "on":
+                    GPIO.output(Relais,GPIO.HIGH)
+                    relaisstatus = "on"
+                    logging.info("Relais status changed: %s", relaisstatus)
+        elif relais == "manual":
+            if GPIO.input(RelaisOn) == True:
+                if relaisstatus != "on":
+                    GPIO.output(Relais,GPIO.HIGH)
+                    relaisstatus = "on"
+                    logging.warning("Relais status changed manually: %s", relaisstatus)
+            elif GPIO.input(RelaisOff) == True:
+                if door != "off":
+                    GPIO.output(Relais,GPIO.LOW)
+                    relaisstatus = "off"
+                    logging.warning("Relais status changed manually: %s", relaisstatus)
+        elif relais == "time":
+            now = (datetime.now(timezone.utc))
+            relaisontime = datetime.strptime(datetime.now().strftime("%Y-%m-%d-")+config['Relais']['RelaisOnTime']+" +0000",'%Y-%m-%d-%H:%M %z')
+            relaisofftime = datetime.strptime(datetime.now().strftime("%Y-%m-%d-")+config['Relais']['RelaisOffTime']+" +0000",'%Y-%m-%d-%H:%M %z')
+            relaisontimetomorrow = relaisontime + timedelta(days=1)
+            relaisofftimeyesterday = relaisofftime + timedelta(days=-1)
+            if GPIO.input(RelaisOn) == True:
+                if relaisstatus != "on":
+                    GPIO.output(Relais,GPIO.HIGH)
+                    relaisstatus = "on"
+                    logging.warning("Relais status changed manually: %s", relaisstatus)
+            elif GPIO.input(RelaisOff) == True:
+                if relaisstatus != "off":
+                    GPIO.output(Relais,GPIO.LOW)
+                    relaisstatus = "off"
+                    logging.warning("Relais status changed manually: %s", relaisstatus)
+            elif relaisontime < now < relaisofftime:
+                if relaisstatus != "on":
+                    GPIO.output(Relais,GPIO.HIGH)
+                    relaisstatus = "on"
+                    logging.warning("Relais status changed: %s", relaisstatus)
+            elif relaisofftimeyesterday < now < relaisontime or relaisofftime < now < relaisontimetomorrow:
+                if relaisstatus != "off":
+                    GPIO.output(Relais,GPIO.LOW)
+                    relaisstatus = "off"
+                    logging.warning("Relais status changed: %s", relaisstatus)
+        elif stop_threads:
+            break
 
-def main_loop():
-    while True:
-        door()
+def main():
+    t1 = threading.Thread(target=door)
+    t1.start()
+    t2 = threading.Thread(target=relais)
+    t2.start()
 
 if __name__ == "__main__":
     try:
-#        t = threading.Thread(target=relais())
- #       t.start()
-        door()
+        main()
     except RuntimeError as error:
         print(error.args[0])
     except KeyboardInterrupt:
         print("\nExiting application\n")
         # exit the applications
+        stop_threads = True
+        t1.join()
+        t.join()
         GPIO.cleanup()
